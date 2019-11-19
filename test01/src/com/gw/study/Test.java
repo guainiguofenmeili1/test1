d50475dc2cc450c86c0e3a07e5b0c84a57771fee
guanw <1160239161@qq.com>
2019-11-20 00:29:36 +0800

    Initial commit

---
+++ test01/src/com/gw/study/Test.java
@@ -0,0 +1,21 @@
+package com.gw.study;
+
+import java.util.Date;
+
+public class Test {
+
+public  static  void main(String[] args){
+    System.out.println("aaa1122212");
+    Date date = new Date();
+    int[] values = {1, 2, 3};
+    for (int value : values) {
+        
+    }
+    if (values == null) {
+        
+    }
+
+  
+}
+
+}
---
+++ test01/src/com/gw/study/Test1.java
@@ -0,0 +1,5 @@
+package com.gw.study;
+
+public class Test1 {
+
+}
